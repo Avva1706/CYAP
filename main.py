52a13247a8dcbf98f0f14e25ea7b673c4b18cb12
Avva1706 <146875994+Avva1706@users.noreply.github.com>
2023-11-15 10:58:33 +0300

    zadanie1

---
+++ main.py
@@ -0,0 +1,15 @@
+with open('Fzad11.txt', 'w') as file:
+    print("Ввод данных в файл Fzad1,1.txt. Для завершения введите пустую строку.")
+    while True:
+        line = input()
+        if not line:
+            break
+        file.write(line+'\n')
+with open('Fzad11.txt', 'r') as write_file, open('Fzad12.txt', 'w') as copy_file:
+    linia = write_file.readlines()
+    second_linia = [linia for index, linia in enumerate(linia, start=1) if index % 2 == 0]
+    copy_file.write(second_linia)
+with open('Fzad11.txt', 'rb') as file:
+    bytees1 = len(file.read())
+with open('Fzad11.txt', 'rb') as file:
+    bytees2 = len(file.read())
